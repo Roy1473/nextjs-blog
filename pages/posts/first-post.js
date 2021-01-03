d1efbb98d31e82ff497574dd233c1a3397831fdf
Roy1473 <pgajdmga@gmail.com>
2021-01-03 23:40:32 +0900

    シェアコンポーネント作成

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>Fist post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
