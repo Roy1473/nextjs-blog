42ae2d409d1cfdf26d583e2ef14f32e847512c02
Roy1473 <pgajdmga@gmail.com>
2021-01-11 04:19:16 +0900

    読み込んだファイル表示

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,33 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
 export function getStaticProps() {
-  const getAllPostData = getSortedPostsData();
+  const allPostData = getSortedPostsData();
   return {
     props: {
-      getAllPostData,
+      allPostData,
     },
   };
 }
 
-export default function Home() {
+export default function Home({ allPostData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi!Im a Beginner for Next.js</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}></h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
